be7c00ec9de5483c2d5103943b6710e0bbe698bf
Xingdi (Eric) Yuan <xingdi-eric-yuan@users.noreply.github.com>
2024-11-11 10:53:58 -0500

    use ? to catch item/items

--- froggy/utils.py
+++ froggy/utils.py
@@ -262,10 +262,7 @@ def cleanup_pytest_output(output):
 
 def extract_max_score_from_pytest_output(output):
     # ... collected 25 items
-    try:
-        return int(re.search(r"collected (\d+) items", output).group(1))
-    except:
-        return 1
+    return int(re.search(r"collected (\d+) items?", output).group(1))
 
 
 def extract_reward_from_pytest_output(output):
