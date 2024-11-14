d8926f2a19160f821f32c874cca28f3a6ad2cbe3
Chinmay Singh <chinmay.singh@gmail.com>
2024-11-14 18:34:11 +0000

    repo env

--- froggy/tests/test_workspace.py
+++ froggy/tests/test_workspace.py
@@ -1,4 +1,20 @@
-import pytest
+from unittest.mock import patch
+from froggy.envs import RepoEnv
+from froggy.utils import load_config
+from pathlib import PosixPath
 
+@patch('sys.argv', ['run.py', 'scripts/config.yaml', '--agent', 'cot', '--debug', '-v'])
 def test_workspace():
-    assert 2 + 2 == 4
+    config, args = load_config()
+    config = config[args.agent]
+
+    assert args.config_file == 'scripts/config.yaml'
+    assert args.agent == 'cot'
+    assert args.debug == True
+    assert args.verbose == True
+    
+    env = RepoEnv(**config["env_kwargs"])
+
+    assert isinstance(env.path, PosixPath)
+    assert env.path == PosixPath('data/pytorch')
+    
