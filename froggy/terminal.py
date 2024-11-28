7afeb6caed4fb3a1b67252b35af8fbe6e428c927
Matheus Pereira <matpereira@microsoft.com>
2024-11-28 10:52:15 -0800

    fix docker terminal prepare_command

--- froggy/terminal.py
+++ froggy/terminal.py
@@ -26,6 +26,7 @@ class Terminal:
         setup_commands: list[str] = None,
         env_vars: dict[str, str] = None,
         include_os_env_vars: bool = True,
+        **kwargs,
     ):
         self.setup_commands = setup_commands or []
         self.env_vars = env_vars or {}
@@ -235,6 +236,7 @@ class DockerTerminal(Terminal):
         base_image: str = "ubuntu:latest",
         volumes: dict[str, dict[str:str]] = None,
         include_os_env_vars: bool = False,
+        **kwargs,
         # TODO: dockerfile and/or docker-compose file?
     ):
         """
@@ -297,18 +299,20 @@ class DockerTerminal(Terminal):
             f"docker exec -i {self.container.name} /bin/bash --noprofile --norc"
         )
 
+    def prepare_command(self, entrypoint: list[str]) -> list[str]:
+        """Prepares a shell command by combining setup commands and entrypoint commands.
+        Then wraps the command in a shell call."""
+        if self.setup_commands:
+            entrypoint = self.setup_commands + entrypoint
+        entrypoint = " && ".join(entrypoint)
+        command = shlex.split(f'/bin/bash -c "{entrypoint}"')
+        return command
+
     def run(self, entrypoint: list[str], timeout=None) -> tuple[bool, str]:
         """Run a command in the terminal. Return command status and output."""
-
-        command = [entrypoint] if isinstance(entrypoint, str) else entrypoint
-
-        if self.setup_commands:
-            command = self.setup_commands + [" && "] + entrypoint
-
-        command = " ".join(command)
+        command = self.prepare_command(entrypoint)
 
         # TODO: docker exec_run timeout?
-        command = f'/bin/bash -c "{command}"'
         status, output = self.container.exec_run(
             command,
             workdir=self.working_dir,
@@ -331,9 +335,7 @@ class DockerTerminal(Terminal):
 
     def setup_container(self) -> docker.models.containers.Container:
         # Create and start a container mounting volumes and setting environment variables
-        logger.debug(
-            f"Setting up container with base image: {self.patched_image}"
-        )
+        logger.debug(f"Setting up container with base image: {self.patched_image}")
         container = self.docker_client.containers.run(
             image=self.patched_image,
             command="sleep infinity",  # Keep the container running
