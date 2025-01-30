533be038aba3ae72abe900a6783c108cc5d89897
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2025-01-29 21:53:48 -0500

    Remove solution agent.

--- froggy/agents/__init__.py
+++ froggy/agents/__init__.py
@@ -1,4 +1,4 @@
-from froggy.agents.base import AgentBase, AgentSolution
+from froggy.agents.base import AgentBase
 from froggy.agents.cot import AgentCoT, AgentCoT_NoPDB
 from froggy.agents.tadpole import AgentTadpole
 from froggy.agents.zero_shot import (
