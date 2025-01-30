cf848b4a2f76e9b8fd69288b678e45d2ba4835cc
Xingdi (Eric) Yuan <xingdi-eric-yuan@users.noreply.github.com>
2025-01-30 16:10:14 -0500

    pandas df

---
+++ data/pandas_dataframe/code.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+
+def load_data():
+    print("Loading data...")
+
+    # URL of the Titanic dataset (CSV format)
+    url = "https://gist.githubusercontent.com/teamtom/1af7b484954b2d4b7e981ea3e7a27f24/raw/114fb69dce56b4462a9c3a417e7402330616ad4f/titanic_full.csv"
+
+    # Read the CSV file
+    df = pd.read_csv(url)
+
+    # Display the first few rows of the dataframe
+    print(df.head())
+    return df
+
+def calculate_stats(df):
+    print("\nCalculating statistics...\n")
+
+    # Calculate the median fare paid
+    median_fare = df['Price'].median()
+    print("\nMedian fare paid:\n", median_fare)
