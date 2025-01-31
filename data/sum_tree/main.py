43244f03fd079e31efad1fc472c831b8e0de3d4a
Xingdi (Eric) Yuan <xingdi-eric-yuan@users.noreply.github.com>
2025-01-31 15:01:50 -0500

    more examples

---
+++ data/sum_tree/main.py
@@ -0,0 +1,24 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.sum = 0  # Will store sum of all nodes below this one
+
+
+def build_sum_tree(root):
+    if not root:
+        return 0
+    
+    left_sum = build_sum_tree(root.left)
+    right_sum = build_sum_tree(root.right)
+    
+    root.sum = left_sum + right_sum + root.value
+    return root.sum
+
+
+def print_tree(node, level=0):
+    if node:
+        print("  " * level + f"Value: {node.value}, Sum: {node.sum}")
+        print_tree(node.left, level + 1)
+        print_tree(node.right, level + 1)
