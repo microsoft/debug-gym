c9b8328d70feb49a35fd7ca668ace08f63adb126
Matheus Pereira <matpereira@microsoft.com>
2025-01-31 11:51:19 -0800

    EnvironmentTool abstract class + abstractmethod reset

--- froggy/tools/tests/test_tool.py
+++ froggy/tools/tests/test_tool.py
@@ -5,30 +5,47 @@ from froggy.tools.tool import EnvironmentTool
 
 
 class FakeTool(EnvironmentTool):
-    pass
+    def use(self, action, environment):
+        pass
+
+    def reset(self):
+        pass
 
 
 def test_register_valid_environment():
-    tool = EnvironmentTool()
+    tool = FakeTool()
     env = RepoEnv()
     tool.register(env)
     assert tool.environment == env
 
 
 def test_register_invalid_environment():
-    tool = EnvironmentTool()
+    tool = FakeTool()
     with pytest.raises(ValueError):
         tool.register(object())
 
 
 def test_is_triggered():
-    tool = EnvironmentTool()
+    tool = FakeTool()
     tool.action = "pdb"
     assert tool.is_triggered("pdb breakpoints")
     assert not tool.is_triggered("otherstuff")
 
 
-def test_use_not_implemented():
-    tool = FakeTool()
-    with pytest.raises(NotImplementedError):
-        tool.use("something", None)
+def test_abstract_class():
+    with pytest.raises(TypeError):
+        EnvironmentTool()
+
+
+def test_abstract_methods():
+    class CompletelyFakeTool(EnvironmentTool):
+        pass
+
+    with pytest.raises(
+        TypeError,
+        match=(
+            "Can't instantiate abstract class CompletelyFakeTool "
+            "without an implementation for abstract methods *"
+        ),
+    ):
+        tool = CompletelyFakeTool()
