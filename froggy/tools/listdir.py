d569775c123138007bf8476216acfacab4394607
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2025-02-04 09:01:54 -0800

    Add an optional depth argument to listdir. Fix tests

--- froggy/tools/listdir.py
+++ froggy/tools/listdir.py
@@ -1,4 +1,5 @@
 from os.path import join as pjoin
+from pathlib import Path
 
 from froggy.tools.tool import EnvironmentTool
 from froggy.tools.toolbox import Toolbox
@@ -13,29 +14,43 @@ class ListdirTool(EnvironmentTool):
     def instructions(self):
         assert hasattr(self, "environment")
         instruction = {
-            "template": "```listdir <path/to/subdirectory>```",
-            "description": f"List the file and folder contents of a subdirectory within the working directory, up to a maximum depth {self.environment.dir_tree_depth}.",
+            "template": "```listdir <path/to/subdirectory> <depth>```",
+            "description": f"List the file and folder contents of a subdirectory within the working directory, up to a specified 'depth' (default depth is {self.environment.dir_tree_depth}).",
             "examples": [
                 f"```listdir``` to list the contents of the working directory.",
-                f"```listdir src``` to list the contents of the 'src' subdirectory.",
                 f"```listdir src/util``` to list the contents of the 'util' subdirectory within the 'src' subdirectory.",
+                f"```listdir src 2``` to list the contents of the 'src' subdirectory up to a depth of 2.",
             ],
         }
         return instruction
 
     def use(self, action):
-        clean_action = self.clean_action(action)
-        startpath = pjoin(self.environment.working_dir, clean_action)
-        result = self.environment.directory_tree(root=startpath)
-        return f"Listdir at {clean_action}:\n{result}"
+        try:
+            listdir_path, depth = self.clean_action(action)
+            startpath = pjoin(self.environment.working_dir, listdir_path)
+            result = self.environment.directory_tree(root=startpath, max_depth=depth)
+        except ValueError as e:
+            return str(e)
+
+        return result
 
     def clean_action(self, action):
         listdir_path = action.strip("`").strip()
-        tmp = listdir_path.split(" ", 1)
+        tmp = listdir_path.split(" ")
+        depth = None
         if len(tmp) == 1:
             # e.g., ```listdir```
             listdir_path = "."
-        else:
+        elif len(tmp) == 2:
             # e.g., ```listdir src```
             listdir_path = tmp[1].strip()
-        return listdir_path
+        elif len(tmp) == 3:
+            # e.g., ```listdir src depth```
+            listdir_path = tmp[1].strip()
+            depth = int(tmp[2].strip())
+            if depth <= 0:
+                raise ValueError(f"Depth must be 1 or greater: {depth}")
+        else:
+            raise ValueError(f"Invalid action (too many arguments): {action}")
+
+        return listdir_path, depth
