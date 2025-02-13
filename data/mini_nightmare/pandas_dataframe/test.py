e6e733e0e3ce8b6a23f57241ad0d10b0edf82a32
Xingdi (Eric) Yuan <xingdi-eric-yuan@users.noreply.github.com>
2025-02-13 11:33:40 -0500

    update mini nightmare data (#56)

--- data/mini_nightmare/pandas_dataframe/test.py
+++ data/mini_nightmare/pandas_dataframe/test.py
@@ -1,4 +1,8 @@
 from code import calculate_stats, load_data
 
-df = load_data()
-calculate_stats(df)
+
+def test_calculate_stats():
+    df = load_data()
+    median_price = calculate_stats(df)
+
+    assert median_price == 14.4542
