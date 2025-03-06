2ffd994f6c5d8812db325382c2e6fc780f55ff67
Xingdi (Eric) Yuan <xingdi-eric-yuan@users.noreply.github.com>
2025-03-05 20:04:03 -0500

    current version

--- analysis/figure_episode_length_and_response_token.py
+++ analysis/figure_episode_length_and_response_token.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from tqdm import tqdm
 
 plt.rcParams.update(
     {
@@ -14,7 +15,7 @@ plt.rcParams.update(
         "axes.titlesize": 20,  # Plot title
         "xtick.labelsize": 20,  # X-axis tick labels
         "ytick.labelsize": 20,  # Y-axis tick labels
-        "legend.fontsize": 14,  # Legend text
+        "legend.fontsize": 20,  # Legend text
     }
 )
 
@@ -74,12 +75,12 @@ def analyze_froggy_results(model_name):
 
     df = pd.DataFrame(results)
 
-    print("Success rate:", df["success"].mean())
-    print("Average rewrites:", df["rewrite_count"].mean())
-    print("Average prompt tokens:", df["prompt_tokens"].mean())
-    print("Average response tokens:", df["response_tokens"].mean())
-    print("\nResults by task:")
-    print(df)
+    # print("Success rate:", df["success"].mean())
+    # print("Average rewrites:", df["rewrite_count"].mean())
+    # print("Average prompt tokens:", df["prompt_tokens"].mean())
+    # print("Average response tokens:", df["response_tokens"].mean())
+    # print("\nResults by task:")
+    # print(df)
     return df
 
 
@@ -115,9 +116,9 @@ def analyze_froggy_results_with_seeds(base_model_name, seeds=[0, 1, 2]):
         .reset_index()
     )
 
-    print(f"\nAveraged results for {base_model_name}:")
-    print(f"Success rate: {averaged_df['success'].mean():.2%}")
-    print(f"Average rewrites: {averaged_df['rewrite_count'].mean():.2f}")
+    # print(f"\nAveraged results for {base_model_name}:")
+    # print(f"Success rate: {averaged_df['success'].mean():.2%}")
+    # print(f"Average rewrites: {averaged_df['rewrite_count'].mean():.2f}")
 
     return combined_df
 
@@ -141,6 +142,8 @@ def plot_episode_length(df_dict, figsize=(12, 7)):
     all_data = []
     # Create plot for each model
     for model_name, df in df_dict.items():
+        # ignore the data points where the agent failed
+        df = df[df["success"]]
         for agent in ["rewrite", "pdb", "seq"]:
             if agent not in model_name:
                 continue
@@ -176,27 +179,64 @@ def plot_episode_length(df_dict, figsize=(12, 7)):
         color="black",
     )
 
-    plt.ylim(0, 60)
+    plt.ylim(0, 30)
     plt.ylabel("Average Episode Length")
     plt.xlabel("Backbone LLM")
     plt.title("Average Episode Lengths (Averaged Across 3 Runs)")
-    plt.xticks(rotation=45)
+    plt.xticks(rotation=90)
     # custom x ticks
     plt.xticks(
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+        [
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            15,
+            16,
+            17,
+            18,
+            19,
+            20,
+            21,
+            22,
+            23,
+        ],
         [
             "4o-mini",
             "4o",
             "o1",
             "o3-mini",
+            "llama32-3b",
+            "llama33-70b",
+            "r1-distill-llama-70b",
+            "r1-distill-qwen-32b",
             "4o-mini",
             "4o",
             "o1",
             "o3-mini",
+            "llama32-3b",
+            "llama33-70b",
+            "r1-distill-llama-70b",
+            "r1-distill-qwen-32b",
             "4o-mini",
             "4o",
             "o1",
             "o3-mini",
+            "llama32-3b",
+            "llama33-70b",
+            "r1-distill-llama-70b",
+            "r1-distill-qwen-32b",
         ],
     )
     # # cutsom legend with same three colors as above
@@ -217,11 +257,12 @@ def plot_episode_response_tokens(df_dict, figsize=(12, 7)):
         df_dict (dict): Dictionary mapping model names to their DataFrames with averaged results
         figsize (tuple): Figure size (width, height)
     """
-    plt.figure(figsize=figsize)
 
     all_data = []
     # Create plot for each model
     for model_name, df in df_dict.items():
+        # ignore the data points where the agent failed
+        df = df[df["success"]]
         for agent in ["rewrite", "pdb", "seq"]:
             if agent not in model_name:
                 continue
@@ -243,12 +284,34 @@ def plot_episode_response_tokens(df_dict, figsize=(12, 7)):
         all_data, columns=["name", "model", "agent", "response_tokens", "std"]
     )
     # import pdb; pdb.set_trace()
-    # bar chart
+    # bar chart, with broken y-axis (0-1000) and (3000-7000)
+    f, (ax1, ax2) = plt.subplots(ncols=1, nrows=2, sharex=True)
     sns.barplot(
-        data=all_data, x="name", y="response_tokens", hue="agent", palette="Set2"
+        data=all_data,
+        x="name",
+        y="response_tokens",
+        hue="agent",
+        palette="Set2",
+        ax=ax1,
+    )
+    sns.barplot(
+        data=all_data,
+        x="name",
+        y="response_tokens",
+        hue="agent",
+        palette="Set2",
+        ax=ax2,
     )
     # add error bars
-    plt.errorbar(
+    ax1.errorbar(
+        x=all_data["name"],
+        y=all_data["response_tokens"],
+        yerr=all_data["std"],
+        fmt="none",
+        capsize=5,
+        color="black",
+    )
+    ax2.errorbar(
         x=all_data["name"],
         y=all_data["response_tokens"],
         yerr=all_data["std"],
@@ -257,35 +320,82 @@ def plot_episode_response_tokens(df_dict, figsize=(12, 7)):
         color="black",
     )
 
-    plt.ylim(0, 1000)
-    plt.ylabel("Average Response Tokens")
+    ax1.set_ylim(2000, 10000)
+    ax2.set_ylim(0, 1000)
+
+    ax1.set_ylabel("")
+    ax2.set_ylabel("")
+    # then, set a new label on the plot (basically just a piece of text) and move it to where it makes sense (requires trial and error)
+    f.text(
+        0.05,
+        0.55,
+        "Average Response Tokens (Averaged Across 3 Runs)",
+        va="center",
+        rotation="vertical",
+    )
+    f.subplots_adjust(
+        left=0.09, right=0.99, bottom=0.31, top=0.97, hspace=0.08, wspace=0.2
+    )
+    ax2.get_legend().remove()
     plt.xlabel("Backbone LLM")
-    plt.title("Average Response Tokens (Averaged Across 3 Runs)")
-    plt.xticks(rotation=45)
+    # plt.title("Average Response Tokens (Averaged Across 3 Runs)")
+    plt.xticks(rotation=90)
     # custom x ticks
     plt.xticks(
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+        [
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            15,
+            16,
+            17,
+            18,
+            19,
+            20,
+            21,
+            22,
+            23,
+        ],
         [
             "4o-mini",
             "4o",
             "o1",
             "o3-mini",
+            "llama32-3b",
+            "llama33-70b",
+            "r1-distill-llama-70b",
+            "r1-distill-qwen-32b",
             "4o-mini",
             "4o",
             "o1",
             "o3-mini",
+            "llama32-3b",
+            "llama33-70b",
+            "r1-distill-llama-70b",
+            "r1-distill-qwen-32b",
             "4o-mini",
             "4o",
             "o1",
             "o3-mini",
+            "llama32-3b",
+            "llama33-70b",
+            "r1-distill-llama-70b",
+            "r1-distill-qwen-32b",
         ],
     )
-    # # cutsom legend with same three colors as above
-    # plt.legend(
-    #     ["rewrite", "debug", "second-chance"],
-    #     loc="upper left",
-    #     bbox_to_anchor=(1, 1),
-    # )
+
     plt.grid(True, alpha=0.3)
     plt.tight_layout()
     plt.show()
@@ -293,25 +403,37 @@ def plot_episode_response_tokens(df_dict, figsize=(12, 7)):
 
 # Example usage:
 model_names = [
-    "../exps/aider/rewrite_4o-mini",
-    "../exps/aider/rewrite_4o",
-    "../exps/aider/rewrite_o1",
-    "../exps/aider/rewrite_o3-mini",
-    "../exps/aider/pdb_4o-mini",
-    "../exps/aider/pdb_4o",
-    "../exps/aider/pdb_o1",
-    "../exps/aider/pdb_o3-mini",
-    "../exps/aider/seq_4o-mini",
-    "../exps/aider/seq_4o",
-    "../exps/aider/seq_o1",
-    "../exps/aider/seq_o3-mini",
+    "../exps/aider/rewrite_4o-mini/rewrite_4o-mini",
+    "../exps/aider/rewrite_4o/rewrite_4o",
+    "../exps/aider/rewrite_o1/rewrite_o1",
+    "../exps/aider/rewrite_o3-mini/rewrite_o3-mini",
+    "../exps/aider/rewrite_llama32-3b/rewrite_llama32-3b",
+    "../exps/aider/rewrite_llama33-70b/rewrite_llama33-70b",
+    "../exps/aider/rewrite_r1-distill-llama-70b/rewrite_r1-distill-llama-70b",
+    "../exps/aider/rewrite_r1-distill-qwen-32b/rewrite_r1-distill-qwen-32b",
+    "../exps/aider/pdb_4o-mini/pdb_4o-mini",
+    "../exps/aider/pdb_4o/pdb_4o",
+    "../exps/aider/pdb_o1/pdb_o1",
+    "../exps/aider/pdb_o3-mini/pdb_o3-mini",
+    "../exps/aider/pdb_llama32-3b/pdb_llama32-3b",
+    "../exps/aider/pdb_llama33-70b/pdb_llama33-70b",
+    "../exps/aider/pdb_r1-distill-llama-70b/pdb_r1-distill-llama-70b",
+    "../exps/aider/pdb_r1-distill-qwen-32b/pdb_r1-distill-qwen-32b",
+    "../exps/aider/seq_4o-mini/seq_4o-mini",
+    "../exps/aider/seq_4o/seq_4o",
+    "../exps/aider/seq_o1/seq_o1",
+    "../exps/aider/seq_o3-mini/seq_o3-mini",
+    "../exps/aider/seq_llama32-3b/seq_llama32-3b",
+    "../exps/aider/seq_llama33-70b/seq_llama33-70b",
+    "../exps/aider/seq_r1-distill-llama-70b/seq_r1-distill-llama-70b",
+    "../exps/aider/seq_r1-distill-qwen-32b/seq_r1-distill-qwen-32b",
 ]
 
 # Analyze all models with seed averaging
-results_dict = {
-    name.split("/")[-1]: analyze_froggy_results_with_seeds(name) for name in model_names
-}
+results_dict = {}
+for name in tqdm(model_names):
+    results_dict[name.split("/")[-1]] = analyze_froggy_results_with_seeds(name)
 
 # Plot comparison
-# plot_episode_length(results_dict)
-plot_episode_response_tokens(results_dict)
+plot_episode_length(results_dict)
+# plot_episode_response_tokens(results_dict)
