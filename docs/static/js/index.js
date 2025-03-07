60ca8b84e929dced5825af9317f232eb724777e1
Chinmay Singh <chsingh@microsoft.com>
2025-03-07 04:49:05 +0000

    consent requirements

--- docs/static/js/index.js
+++ docs/static/js/index.js
@@ -19,3 +19,38 @@ $(document).ready(function() {
     bulmaSlider.attach();
 
 })
+
+  function onConsentChanged() {
+	// function gtag() {
+	//   window.dataLayer.push(arguments)
+	// }
+  
+	// if (!consentRequired() || WcpConsent.siteConsent.getConsentFor(WcpConsent.consentCategories.Analytics)) {
+	//   // Load GA
+	//   loadAnalytics(gtag)
+	// }
+  }
+
+  function consentRequired() {
+	return WcpConsent.siteConsent.isConsentRequired;
+  }
+  
+  $(function() {
+	// Load GA upfront because we classify it as essential cookie
+	window.dataLayer = window.dataLayer || []
+	function gtag() {
+	  dataLayer.push(arguments)
+	}
+	gtag('js', new Date())
+  
+	window.WcpConsent && WcpConsent.init("en-US", "cookie-banner", function (err, _siteConsent) {
+	}, onConsentChanged, WcpConsent.themes.light);
+  
+	// const cookieManager = document.querySelector('#footer-cookie-link');
+	// if (consentRequired() && cookieManager && cookieManager.parentElement) {
+	//   cookieManager.parentElement.style.visibility = 'visible';
+	// }
+  
+	// initialize consent
+	onConsentChanged();
+  })
