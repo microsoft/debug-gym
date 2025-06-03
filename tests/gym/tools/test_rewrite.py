28fce64ee0b5484951a234835464eee9ef60bea7
Matheus Pereira <matpereira@microsoft.com>
2025-06-03 09:39:07 -0700

    Let RepoEnv handle file checks. Refactor RewriteTool for improved file handling and error messaging

--- tests/gym/tools/test_rewrite.py
+++ tests/gym/tools/test_rewrite.py
@@ -12,12 +12,13 @@ def env(tmp_path):
     repo_path = tmp_path / "repo"
     repo_path.mkdir()
 
-    file_content = """import abc
-
-def greet():
-    print('Hello, world!')
-    print('Goodbye, world!')
-"""
+    file_content = (
+        "import abc\n"
+        "\n"
+        "def greet():\n"
+        "    print('Hello, world!')\n"
+        "    print('Goodbye, world!')\n"
+    )
 
     with open(repo_path / "test.py", "w") as f:
         f.write(file_content)
@@ -41,15 +42,11 @@ def test_rewrite_no_path_error(env):
     }
     obs = rewrite_tool.use(env, **patch)
     assert obs.source == "rewrite"
-    assert (
-        obs.observation
-        == "File path is None. Please provide a valid file path.\nRewrite failed."
-    )
+    assert obs.observation == "Rewrite failed. Error message:\nFile path is None.\n"
 
 
 def test_rewrite_with_file_path(env):
     rewrite_tool = env.get_tool("rewrite")
-
     patch = {
         "path": "test.py",
         "start": 4,
@@ -60,32 +57,29 @@ def test_rewrite_with_file_path(env):
 
     assert rewrite_tool.rewrite_success
     # using \n to prevent ide from removing trailing spaces
-    assert (
-        obs.observation
-        == """The file `test.py` has been updated successfully.
-
-Diff:
-
---- original
-+++ current
-@@ -1,5 +1,5 @@
- import abc
- \n def greet():
--    print('Hello, world!')
-+    print(f'Hello, {name}!')
-     print('Goodbye, world!')
-"""
+    assert obs.observation == (
+        "The file `test.py` has been updated successfully.\n"
+        "\n"
+        "Diff:\n"
+        "\n"
+        "--- original\n"
+        "+++ current\n"
+        "@@ -1,5 +1,5 @@\n"
+        " import abc\n"
+        " \n"
+        " def greet():\n"
+        "-    print('Hello, world!')\n"
+        "+    print(f'Hello, {name}!')\n"
+        "     print('Goodbye, world!')\n"
     )
     with open(env.working_dir / "test.py", "r") as f:
         new_content = f.read()
-    assert (
-        new_content
-        == """import abc
-
-def greet():
-    print(f'Hello, {name}!')
-    print('Goodbye, world!')
-"""
+    assert new_content == (
+        "import abc\n"
+        "\n"
+        "def greet():\n"
+        "    print(f'Hello, {name}!')\n"
+        "    print('Goodbye, world!')\n"
     )
 
 
@@ -102,37 +96,28 @@ def test_rewrite_start_end(env):
 
     assert rewrite_tool.rewrite_success
     # using \n to prevent ide from removing trailing spaces
-    assert (
-        obs.observation
-        == """The file `test.py` has been updated successfully.
-
-Diff:
-
---- original
-+++ current
-@@ -1,5 +1,4 @@
- import abc
- \n def greet():
--    print('Hello, world!')
--    print('Goodbye, world!')
-+    print(f'Hello, {name}!')
-"""
+    assert obs.observation == (
+        "The file `test.py` has been updated successfully.\n"
+        "\n"
+        "Diff:\n"
+        "\n"
+        "--- original\n"
+        "+++ current\n"
+        "@@ -1,5 +1,4 @@\n"
+        " import abc\n"
+        " \n def greet():\n"
+        "-    print('Hello, world!')\n"
+        "-    print('Goodbye, world!')\n"
+        "+    print(f'Hello, {name}!')\n"
     )
     with open(env.working_dir / "test.py", "r") as f:
         new_content = f.read()
-    assert (
-        new_content
-        == """import abc
-
-def greet():
-    print(f'Hello, {name}!')
-"""
-    )
+    assert new_content == ("import abc\n\ndef greet():\n    print(f'Hello, {name}!')\n")
 
 
 def test_full_rewrite(env):
     rewrite_tool = env.get_tool("rewrite")
-
+    # env.editable_files = [f"{env.working_dir}/test.py"]
     patch = {
         "path": "test.py",
         "new_code": "print(f'Hello, {name}!')",
@@ -140,22 +125,20 @@ def test_full_rewrite(env):
     obs = rewrite_tool.use(env, **patch)
 
     assert rewrite_tool.rewrite_success
-    # using \n to prevent ide from removing trailing spaces
-    assert (
-        obs.observation
-        == """The file `test.py` has been updated successfully.
-
-Diff:
-
---- original
-+++ current
-@@ -1,5 +1 @@
--import abc
--
--def greet():
--    print('Hello, world!')
--    print('Goodbye, world!')
-+print(f'Hello, {name}!')"""
+    assert obs.observation == (
+        "The file `test.py` has been updated successfully.\n"
+        "\n"
+        "Diff:\n"
+        "\n"
+        "--- original\n"
+        "+++ current\n"
+        "@@ -1,5 +1 @@\n"
+        "-import abc\n"
+        "-\n"
+        "-def greet():\n"
+        "-    print('Hello, world!')\n"
+        "-    print('Goodbye, world!')\n"
+        "+print(f'Hello, {name}!')"
     )
     with open(env.working_dir / "test.py", "r") as f:
         new_content = f.read()
@@ -163,22 +146,27 @@ Diff:
 
 
 def test_rewrite_invalid_file(env):
+    # overwrite the is_editable method to simulate a read-only file
+    env.is_editable = lambda x: x != "read_only.py"
     rewrite_tool = env.get_tool("rewrite")
-    env.all_files = ["another_file.py"]
-
     patch = {
-        "path": "test.py",
+        "path": "another_file.py",
         "start": 2,
         "end": None,
         "new_code": "    print(f'Hello, {name}!')",
     }
     obs = rewrite_tool.use(env, **patch)
-
-    assert (
-        obs.observation
-        == "Error while rewriting the file: File test.py does not exist or is not in the current repository.\nRewrite failed."
+    assert obs.observation == (
+        "Rewrite failed. Error message:\n"
+        "File `another_file.py` does not exist or is not "
+        f"in the working directory `{env.working_dir}`.\n"
     )
-    assert not rewrite_tool.rewrite_success
+
+    patch["path"] = "read_only.py"
+    obs = rewrite_tool.use(env, **patch)
+    assert obs.observation == (
+        "Rewrite failed. Error message:\nFile `read_only.py` is not editable.\n"
+    )
 
 
 def test_rewrite_invalid_line_number(env):
@@ -192,9 +180,9 @@ def test_rewrite_invalid_line_number(env):
     }
     obs = rewrite_tool.use(env, **patch)
 
-    assert (
-        obs.observation
-        == "Invalid line number, line numbers are 1-based.\nRewrite failed."
+    assert obs.observation == (
+        "Rewrite failed. Error message:\n"
+        "Invalid line number, line numbers are 1-based.\n"
     )
     assert not rewrite_tool.rewrite_success
 
@@ -210,8 +198,8 @@ def test_rewrite_invalid_line_number_2(env):
     }
     obs = rewrite_tool.use(env, **patch)
 
-    assert (
-        obs.observation
-        == "Invalid line number range, start should be less than or equal to end.\nRewrite failed."
+    assert obs.observation == (
+        "Rewrite failed. Error message:\n"
+        "Invalid line number range, start should be less than or equal to end.\n"
     )
     assert not rewrite_tool.rewrite_success
