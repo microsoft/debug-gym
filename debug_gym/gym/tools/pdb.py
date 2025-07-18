fb1db63931c8b04823653de521cdf172c2cca910
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2025-07-18 11:50:02 -0700

    Clean up comment

--- debug_gym/gym/tools/pdb.py
+++ debug_gym/gym/tools/pdb.py
@@ -156,7 +156,6 @@ class PDBTool(EnvironmentTool):
         else:  # other pdb commands, send directly
             try:
                 pdb_out = self.interact_with_pdb(command, environment.run_timeout)
-                # remove the working dir from the output
                 if pdb_out in (
                     "End of file",
                     "Blank or comment",
