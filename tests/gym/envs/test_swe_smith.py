7e92e4ea847677ccd99e1befb5ee26c9c74446c7
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2026-03-11 11:17:12 -0700

    Better deal with parallel tests

--- tests/gym/envs/test_swe_smith.py
+++ tests/gym/envs/test_swe_smith.py
@@ -129,7 +129,7 @@ def test_reset_and_step(get_swe_smith_env):
     env_info = env.step(tool_call)
     assert env_info.step_observation == Observation(
         source="env",
-        observation="Unregistered tool: listdir",
+        observation="Tool 'listdir' not found among available tools: eval.",
     )
 
     listdir_tool = Toolbox.get_tool("listdir")
